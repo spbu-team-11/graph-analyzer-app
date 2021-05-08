a102e09aa5df1103bd960da527c002bfc5e5b2aa
ViktorKarasss <70607580+ViktorKarasss@users.noreply.github.com>
2021-05-08 20:39:31 +0300

    Added Alerter

---
+++ src/main/kotlin/utils/Alerter.kt
@@ -0,0 +1,16 @@
+package utils
+
+import javafx.scene.control.Alert
+import tornadofx.alert
+
+class Alerter {
+
+    fun alertHelp(){
+        alert(Alert.AlertType.INFORMATION, "Ссылка на гит с инфой")
+    }
+
+    fun alertIncorrectArgs(message: String){
+        alert(Alert.AlertType.ERROR, message)
+    }
+
+}
