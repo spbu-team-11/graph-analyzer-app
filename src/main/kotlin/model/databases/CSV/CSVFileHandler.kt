030eb87ecdcc3acee2537f968d450eecd0a84dc4
ShishaK1N9 <st077017@student.spbu.ru>
2021-05-13 12:28:43 +0300

    Added some methods

--- src/main/kotlin/model/databases/CSV/CSVFileHandler.kt
+++ src/main/kotlin/model/databases/CSV/CSVFileHandler.kt
@@ -4,6 +4,7 @@ import view.GraphView
 import model.UndirectedGraph
 import model.databases.CSV.data.CSVGraphData
 import model.databases.CSV.data.VertexViewData
+import utils.Alerter
 
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
@@ -13,40 +14,12 @@ import javafx.scene.text.Text
 import java.io.File
 
 @ExperimentalStdlibApi
-class CSVFileHandler() {
+class CSVFileHandler {
 
     fun save(file: File, graphView: GraphView) {
         val data: MutableList<MutableList<String>> = mutableListOf(mutableListOf())
-
-        var i = 0
-        graphView.vertices().onEach {
-            data.add(
-                mutableListOf(
-                    "true",
-                    it.vertex.element,
-                    it.centerX.toString(),
-                    it.centerY.toString(),
-                    it.color.red.toString() + "/" + it.color.green.toString() + "/" + it.color.blue.toString(),
-                    it.community.text.toString(),
-                    "",
-                    ""
-                )
-            )
-        }
-        graphView.edges().onEach {
-            data.add(
-                mutableListOf(
-                    "false",
-                    it.label.text,
-                    "",
-                    "",
-                    "",
-                    "",
-                    it.first.vertex.element,
-                    it.second.vertex.element
-                )
-            )
-        }
+        graphView.addVerticesToData(data)
+        graphView.addEdgesToData(data)
 
         val csvWriter = csvWriter { delimiter = ',' }
         val header = listOf("isNode", "name", "x", "y", "color", "community", "from", "to")
@@ -55,37 +28,74 @@ class CSVFileHandler() {
         csvWriter.writeAll(data, file, append = true)
     }
 
-    fun open(file: File): Pair<UndirectedGraph, GraphView> {
-        val reader = csvReader { skipEmptyLine = true }
-        val csvContents = reader.readAllWithHeader(file)
-        val data = grass<CSVGraphData>().harvest(csvContents)
+    fun open(file: File): Pair<UndirectedGraph, GraphView?> {
+        try {
+            val reader = csvReader { skipEmptyLine = true }
+            val csvContents = reader.readAllWithHeader(file)
+            val data = grass<CSVGraphData>().harvest(csvContents)
 
-        val vertices = hashMapOf<String, VertexViewData>()
-        val newGraph = UndirectedGraph()
-        data.onEach {
-            if (it.isNode) {
-                newGraph.addVertex(it.name)
-                val rgb = it.color!!.split("/").map { color -> color.toDouble() }
-                val vertex = VertexViewData(
-                    it.x!!,
-                    it.y!!,
-                    Text(it.community?.toString() ?: "-1"),
-                    Color.color(rgb[0], rgb[1], rgb[2])
-                )
-                vertices[it.name] = vertex
+            val vertices = hashMapOf<String, VertexViewData>()
+            val newGraph = UndirectedGraph()
+            data.onEach {
+                if (it.isNode) {
+                    newGraph.addVertex(it.name)
+                    val rgb = it.color!!.split("/").map { color -> color.toDouble() }
+                    val vertex = VertexViewData(
+                        it.x!!,
+                        it.y!!,
+                        Text(it.community?.toString() ?: "-1"),
+                        Color.color(rgb[0], rgb[1], rgb[2])
+                    )
+                    vertices[it.name] = vertex
+                }
+
             }
+            data.onEach { if (!it.isNode) newGraph.addEdge(it.from!!, it.to!!, it.name) }
 
+            val newGraphView = GraphView(newGraph)
+            newGraphView.vertices().onEach {
+                val vertex = vertices[it.vertex.element]!!
+                it.centerX = vertex.x
+                it.centerY = vertex.y
+                it.community = vertex.community
+                it.color = vertex.color
+            }
+            return newGraph to newGraphView
+        } catch (e: Exception) {
+            Alerter().alertIncorrectArgs("Incorrect .csv file")
+            return UndirectedGraph() to null
         }
-        data.onEach { if (!it.isNode) newGraph.addEdge(it.from!!, it.to!!, it.name) }
+    }
 
-        val newGraphView = GraphView(newGraph)
-        newGraphView.vertices().onEach {
-            val vertex = vertices[it.vertex.element]!!
-            it.centerX = vertex.x
-            it.centerY = vertex.y
-            it.community = vertex.community
-            it.color = vertex.color
+    private fun GraphView.addVerticesToData(data: MutableList<MutableList<String>>) {
+        vertices().onEach {
+            val csvRow = mutableListOf(
+                "true",
+                it.vertex.element,
+                it.centerX.toString(),
+                it.centerY.toString(),
+                it.color.red.toString() + "/" + it.color.green.toString() + "/" + it.color.blue.toString(),
+                it.community.text.toString(),
+                "",
+                ""
+            )
+            data.add(csvRow)
         }
-        return newGraph to newGraphView
+    }
+
+    private fun GraphView.addEdgesToData(data: MutableList<MutableList<String>>) {
+        edges().onEach {
+            val csvRow = mutableListOf(
+                "false",
+                it.label.text,
+                "",
+                "",
+                "",
+                "",
+                it.first.vertex.element,
+                it.second.vertex.element
+            )
+            data.add(csvRow)
+        }
     }
 }
