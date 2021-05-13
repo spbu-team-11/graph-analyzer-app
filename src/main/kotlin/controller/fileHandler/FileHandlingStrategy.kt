5b50a03b18c2257563320802f81a641aee588f97
ViktorKarasss <70607580+ViktorKarasss@users.noreply.github.com>
2021-05-13 06:03:07 +0300

    removed sqlite module from force-atlas and add sqlite support

--- src/main/kotlin/controller/fileHandler/FileHandlingStrategy.kt
+++ src/main/kotlin/controller/fileHandler/FileHandlingStrategy.kt
@@ -1,10 +1,11 @@
 package controller.fileHandler
 
 import model.Graph
+import model.UndirectedGraph
 import view.GraphView
 import java.io.File
 
 interface FileHandlingStrategy<V ,E> {
-    fun open(file: File, graph: Graph<V, E>, graphView: GraphView<V, E>)
-    fun save(file: File, graph: Graph<String, Long>, graphView: GraphView<V, E>)
+    fun open(file: File): Pair<UndirectedGraph<String, Long>, GraphView<String, Long>?>
+    fun save(file: File, graph: Graph<String, Long>, graphView: GraphView<String, Long>)
 }
